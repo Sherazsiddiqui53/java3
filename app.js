16c5e1ea5e0b3257219a4a0aa727f6c9230b166e
sherazsidd2004 <sherazsidd2004@gmail.com>
2023-08-28 19:26:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+       //  ARRAYS
+
+// var studentNames = [];
+// var FutureStudents ={};
+// var stringarray = ["banana","apple","orange","date"];
+// var numberarray = [2,3,4,5,6];
+// var booleanarray = [true,false];
+// var mixedarray = [banana,true,2,cherry,false,3];
+ 
+
+// var qualification = [
+//        "SSC",
+//       "HSC",
+//       "BCS",
+//       "BS",
+//       "BCOM",
+//       "MS",
+//       "M. Phil.",
+//       "PhD"
+// ];
+
+// var studentNames = ["Student1", "Student2", "Student3"];
+// var studentScores = [430, 380, 450];
+// function calculatePercentage(score) {
+//        return (score / 500) * 100;
+//    }function calculatePercentage(score) {
+//        return (score / 500) * 100;
+//    }
+
+// var colorArray = ['green','blue','red'];
+// documentArray = ('intial array:' + colorArray + '<br><br>' );
+
+// var colorToAddBegining = prompt("Enter a color to add to the begning");
+// colorArray.unshift(colorToAddBeginning);
+// document.write("After adding color to the beginning: " + colorArray + "<br><br>");
+
+// var colorToAddEnd =prompt("Enter a color to Add to the end");
+// colorArray.push(colorToAddEnd);
+// document.write("After adding color to the end" + color + "<br><br>");
+
+// colorArray.unshift("Purple", "Yellow");
+// document.write("After adding two colors to the beginning: " + colorArray + "<br><br>");
+
+// colorArray.shift();
+// document.write("Ahter Deleting the first color" + colorArray + '<br><br>' );
+
+// colorArray.pop();
+// document.write("After deleting the last color: " + colorArray + "<br><br>");
+
+// var indexToAdd = prompt("Enter the index to add a color:");
+// var colorToAddIndex = prompt("Enter the color to add:");
+// colorArray.splice(indexToAdd, 0, colorToAddIndex);
+// document.write("After adding color at index " + indexToAdd + ": " + colorArray + "<br><br>");
+
+// var indexToDelete = prompt("Enter the index from which you want to delete color(s):");
+// var numColorsToDelete = prompt("Enter the number of colors to delete:");
+// colorArray.splice(indexToDelete, numColorsToDelete);
+// document.write("After deleting colors from index " + indexToDelete + ": " + colorArray + "<br><br>");
+
+// let studentScores = [85, 72, 93, 65, 78, 90, 88];
+// studentScores.sort(function(a, b) {
+//        return a - b;
+//      });
+//      console.log("Sorted Student Scores:", studentScores);
+
+// let stack = [];
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+
+// console.log(stack.pop()); 
+// console.log(stack.pop()); 
+// console.log(stack.pop()); 
